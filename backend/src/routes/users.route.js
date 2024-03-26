5569906fa32e2dbd87b8b09288322732ab2c81e5
Kamlesh Bhatt <kamleshbhatt3103@gmail.com>
2024-03-26 18:57:52 +0530

    feat: Added bcrypt to hash the password

--- backend/src/routes/users.route.js
+++ backend/src/routes/users.route.js
@@ -1,9 +1,12 @@
 import express from "express";
 import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
 
 const router = express.Router();
 
 router.post("/register", async (req, res, next) => {
+  const { email, firstName, lastName, password } = req.body;
+
   try {
     let user = await User.findOne({
       email: req.body.email,
@@ -12,9 +15,12 @@ router.post("/register", async (req, res, next) => {
       return res.status(400).json({ message: "User already exists!" });
     }
 
-    user = new User(req.body);
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    user = new User({ email, firstName, lastName, password: hashedPassword });
     await user.save();
-    res.status(200).json({ message: user });
+
+    const { password: pass, ...rest } = user._doc;
+    res.status(201).json({ message: rest });
   } catch (error) {
     console.log("Error in the register route ", error);
     res.status(500).json({ message: "Something went wrong" });
