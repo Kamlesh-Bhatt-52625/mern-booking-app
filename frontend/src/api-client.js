fba2f10eb40342c5202fef309df3eb5e9f18c63e
Kamlesh Bhatt <kamleshbhatt3103@gmail.com>
2024-03-28 20:18:09 +0530

    feat: Connected Register UI with backend

---
+++ frontend/src/api-client.js
@@ -0,0 +1,18 @@
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+//  Register User function
+export const register = async (formData) => {
+  const response = await fetch(`${API_BASE_URL}/api/users/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  });
+
+  const responseBody = await response.json();
+
+  if (!response.ok) {
+    throw new Error(responseBody.message);
+  }
+};
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,4 +1,6 @@
 import { useForm } from "react-hook-form";
+import { useMutation } from "react-query";
+import * as apiClient from "../api-client";
 
 const Register = () => {
   const {
@@ -8,8 +10,17 @@ const Register = () => {
     formState: { errors },
   } = useForm();
 
+  const mutation = useMutation(apiClient.register, {
+    onSuccess: () => {
+      console.log("registration Successfull");
+    },
+    onError: (err) => {
+      console.log(err.message);
+    },
+  });
+
   const onSubmit = handleSubmit((data) => {
-    console.log(data);
+    mutation.mutate(data);
   });
 
   return (
